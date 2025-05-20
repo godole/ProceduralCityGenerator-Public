c2ff97ce80515302c9cf4785ea615a6b5501f0c0
godole <godole199@naver.com>
2025-05-20 16:36:36 +0900

    건물 포지션 테스트용 데이터 추가

---
+++ Assets/Scenes/LoadGeneration/TestBuildingPositionData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBuildingPositionData : ScriptableObject
+{
+    public List<Vector3> Positions;
+}
